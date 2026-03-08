c56156fc30b1d5f0dd4a3575601a7555bd62b60e
JoaoSant7 <pedro_romano7@proton.me>
2026-03-08 13:56:11 -0300

    adicionar kanagawa e kanso ao neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.cmd.colorscheme("kanso-mist")
 vim.opt.wrap = true
 vim.opt.linebreak = true
---
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    priority = 1000,
+    opts = {
+      theme = "wave", -- wave | dragon | lotus
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
