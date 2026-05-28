cd59251c4f23ea60576fd6ddb81d689741abe841
JoaoSant7 <pedro_romano7@proton.me>
2026-05-28 16:52:41 -0300

    revert: the changes to kanagawa color highlighting

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -1,7 +1,10 @@
 return {
-  "rebelot/kanagawa.nvim",
-  config = function()
-    require("kanagawa").setup({
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+
+    opts = {
       compile = false,
       undercurl = true,
 
@@ -26,105 +29,20 @@ return {
       },
 
       overrides = function(colors)
-        local theme = colors.theme
-        local palette = colors.palette
-
-        return {
-          -- Floating windows
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none" },
-          FloatTitle = { bg = "none" },
-
-          -- Darker windows
-          NormalDark = {
-            fg = theme.ui.fg_dim,
-            bg = theme.ui.bg_m3,
-          },
-
-          LazyNormal = {
-            bg = theme.ui.bg_m3,
-            fg = theme.ui.fg_dim,
-          },
-
-          MasonNormal = {
-            bg = theme.ui.bg_m3,
-            fg = theme.ui.fg_dim,
-          },
-
-          -- Syntax groups
-          String = { italic = true },
-
-          Operator = { fg = palette.dragonGray2 },
-
-          -- Static values
-          Boolean = { fg = palette.dragonPink },
-          Constant = { fg = palette.dragonPink },
-          Number = { fg = palette.dragonPink },
-
-          -- Keywords
-          Keyword = { fg = palette.dragonRed },
-
-          -- Functions
-          Function = { fg = palette.dragonOrange },
-          ["@function.builtin"] = { fg = palette.dragonOrange },
-          ["@lsp.type.magicFunction"] = {
-            fg = palette.dragonOrange,
-          },
-          ["@lsp.typemod.function.builtin"] = {
-            fg = palette.dragonOrange,
-          },
-          ["@lsp.typemod.function.defaultLibrary"] = {
-            fg = palette.dragonOrange,
-          },
-          ["@lsp.typemod.method.defaultLibrary"] = {
-            fg = palette.dragonOrange,
-          },
-          ["@lsp.typemod.function.readonly"] = {
-            fg = palette.dragonOrange,
-          },
-
-          -- Types
-          Type = { fg = palette.dragonYellow },
-          Special = { fg = palette.dragonYellow },
-
-          -- Parameters / members
-          Identifier = { fg = palette.dragonBlue },
-          Statement = { fg = palette.dragonBlue },
-          ["@variable.member"] = {
-            fg = palette.dragonBlue,
-          },
-
-          -- Variables
-          ["@namespace"] = {
-            fg = palette.dragonWhite,
-          },
-
-          ["@variable.builtin"] = {
-            fg = palette.dragonRed,
-          },
-
-          ["@variable.parameter"] = {
-            fg = palette.dragonWhite,
-          },
-
-          ["@variable"] = {
-            fg = palette.dragonWhite,
-          },
-
-          ["@attribute"] = {
-            fg = palette.dragonWhite,
-          },
-        }
+        return {}
       end,
 
-      theme = "dragon",
+      theme = "wave",
 
       background = {
-        dark = "dragon",
+        dark = "wave",
         light = "lotus",
       },
-    })
+    },
 
-    vim.cmd("colorscheme kanagawa")
-  end,
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
+      vim.cmd.colorscheme("kanagawa")
+    end,
+  },
 }
