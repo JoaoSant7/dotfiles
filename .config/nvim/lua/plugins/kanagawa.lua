ad91e10d67e824d177afc021ff07f4a6519765c3
JoaoSant7 <pedro_romano7@proton.me>
2026-05-28 21:55:57 -0300

    update: small corretions for kanagawa.lua

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -1,48 +1,32 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-
-    opts = {
-      compile = false,
-      undercurl = true,
-
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("kanagawa").setup({
+      compile = false, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
       commentStyle = { italic = false },
       functionStyle = {},
       keywordStyle = { italic = true },
       statementStyle = { bold = true },
       typeStyle = {},
-
-      transparent = false,
-      dimInactive = false,
-      terminalColors = true,
-
-      colors = {
+      transparent = false, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = { -- add/modify theme and palette colors
         palette = {},
-        theme = {
-          wave = {},
-          lotus = {},
-          dragon = {},
-          all = {},
-        },
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
       },
-
-      overrides = function(colors)
+      overrides = function(colors) -- add/modify highlights
         return {}
       end,
-
-      theme = "wave",
-
-      background = {
-        dark = "wave",
+      theme = "wave", -- Load "wave" theme
+      background = { -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
         light = "lotus",
       },
-    },
-
-    config = function(_, opts)
-      require("kanagawa").setup(opts)
-      vim.cmd.colorscheme("kanagawa")
-    end,
-  },
+    })
+    vim.cmd("colorscheme kanagawa")
+  end,
 }
