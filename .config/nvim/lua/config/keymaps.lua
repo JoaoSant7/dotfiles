613e51374993fe94f02068636d3c170b5c3d14a5
JoaoSant7 <pedro_romano7@proton.me>
2026-05-28 21:55:34 -0300

    add: <leader> + h as a dashboard shortcut nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Makes "jj" a shortcut to Normal Mode
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+
+-- Makes <leader> + h a shortcut to the dashboard
+vim.keymap.set("n", "<leader>h", function()
+  require("snacks").dashboard.open()
+end, { desc = "Open Dashboard" })
