2b3b6cd7c398ab86033bbbd0164d92130d9eb1bf
JoaoSant7 <pedro_romano7@proton.me>
2026-06-07 21:27:12 -0300

    add: sane defaults for WezTerm on wayland

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,25 +1,41 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- startup shell
+config.default_prog = { "/usr/bin/zsh", "-l" }
+
 -- font
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 19
+config.line_height = 1.0
 
--- color scheme
-config.color_scheme = "Kanagawa (Gogh)"
+-- colors
+config.color_scheme = "Kanagawa Dragon (Gogh)"
 
 -- appearance
-config.window_decorations = "RESIZE"
+config.window_decorations = "NONE"
 config.hide_tab_bar_if_only_one_tab = true
-config.bold_brightens_ansi_colors = false
+config.bold_brightens_ansi_colors = true
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 -- performance
+config.front_end = "OpenGL"
+config.prefer_egl = true
 
-config.max_fps = 120
-config.animation_fps = 120
-config.enable_wayland = true
+-- IMPORTANT:
+-- config.enable_wayland = true
 
--- start maximized
+-- reduce rendering overhead
+config.animation_fps = 1
+config.cursor_blink_rate = 0
+
+-- startup
 wezterm.on("gui-startup", function(cmd)
 	local _, _, window = wezterm.mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
