216b73b628000f526599c4cf7341cfc260798412
JoaoSant7 <pedro_romano7@proton.me>
2026-06-08 16:08:22 -0300

    change: supress grep alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -20,7 +20,7 @@ alias cat='bat'
 # Core utilities
 # =========================================================
 
-alias grep='rg --color=auto'
+#alias grep='rg --color=auto'
 alias diff='diff --color=auto'
 alias df='df -h'
 
