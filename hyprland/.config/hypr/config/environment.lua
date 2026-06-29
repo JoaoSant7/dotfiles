f739d89264c7ab5224826ec719dd1841f0c37bd3
JoaoSant7 <pedro_romano7@proton.me>
2026-06-29 08:12:08 -0300

    add: nvidia env var

--- hyprland/.config/hypr/config/environment.lua
+++ hyprland/.config/hypr/config/environment.lua
@@ -1,2 +1,6 @@
 -- Environmental variables
 -- if you don't use UWSM, define your variables here (e.g. hl.env("QT_QPA_PLATFORM", "wayland"))
+
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
