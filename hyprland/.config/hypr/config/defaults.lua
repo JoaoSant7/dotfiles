c8eb420bcb82677e765803d9974456ccb2394c9a
JoaoSant7 <pedro_romano7@proton.me>
2026-06-29 08:12:00 -0300

    change: hyprland defaults

--- hyprland/.config/hypr/config/defaults.lua
+++ hyprland/.config/hypr/config/defaults.lua
@@ -1,7 +1,7 @@
 -- Hyprland default apps
 
 TERMINAL = "ghostty +new-window"
-FILE_MANAGER = "dolphin"
-BROWSER = "brave-origin"
+FILE_MANAGER = "thunar"
+BROWSER = "firefox"
 EDITOR = "gnome-text-editor --new-window"
 CALCULATOR = "gnome-calculator"
