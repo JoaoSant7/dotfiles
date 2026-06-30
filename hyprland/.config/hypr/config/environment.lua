208c857ebfca33813455c84b5e1053bde5c539bd
JoaoSant7 <pedro_romano7@proton.me>
2026-06-30 00:05:39 -0300

    move: env vars to uwsm

--- hyprland/.config/hypr/config/environment.lua
+++ hyprland/.config/hypr/config/environment.lua
@@ -1,12 +1,12 @@
 -- Environmental variables
 -- if you don't use UWSM, define your variables here (e.g. hl.env("QT_QPA_PLATFORM", "wayland"))
 
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
+--hl.env("LIBVA_DRIVER_NAME", "nvidia")
+--hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+--hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
-hl.env("HYPRCURSOR_THEME", "bibata-modern-ice")
-hl.env("HYPRCURSOR_SIZE", "24")
+--hl.env("HYPRCURSOR_THEME", "bibata-modern-ice")
+--hl.env("HYPRCURSOR_SIZE", "24")
 
-hl.env("XCURSOR_THEME", "bibata-modern-ice")
-hl.env("XCURSOR_SIZE", "24")
+--hl.env("XCURSOR_THEME", "bibata-modern-ice")
+--hl.env("XCURSOR_SIZE", "24")
