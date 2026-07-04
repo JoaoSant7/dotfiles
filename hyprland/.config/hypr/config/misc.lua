df3f4e5cfb0cd02fde812e0e7a9c986dd6866068
JoaoSant7 <pedro_romano7@proton.me>
2026-07-03 22:50:15 -0300

    disable: hyprland splash

--- hyprland/.config/hypr/config/misc.lua
+++ hyprland/.config/hypr/config/misc.lua
@@ -13,8 +13,9 @@ hl.config({
 	},
 	misc = {
 		col = {
-			splash = CACHYLGREEN,
+			splash = "0xff95C561",
 		},
+		disable_splash_rendering = true,
 		middle_click_paste = false,
 		enable_swallow = true,
 		swallow_regex = "(kitty|ghostty|[Kk]onsole|Alacritty|gnome-terminal|xfce[0-9]?-terminal)",
