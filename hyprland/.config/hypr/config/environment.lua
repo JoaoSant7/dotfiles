a9750f66c10c9127b64048aad2d41be91ed727e0
JoaoSant7 <pedro_romano7@proton.me>
2026-07-04 17:22:29 -0300

    add: set bibata modern ice as a cursor on hyprland environment

--- hyprland/.config/hypr/config/environment.lua
+++ hyprland/.config/hypr/config/environment.lua
@@ -5,8 +5,8 @@
 --hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 --hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
---hl.env("HYPRCURSOR_THEME", "bibata-modern-ice")
---hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_THEME", "bibata-modern-ice")
+hl.env("HYPRCURSOR_SIZE", "24")
 
---hl.env("XCURSOR_THEME", "bibata-modern-ice")
---hl.env("XCURSOR_SIZE", "24")
+hl.env("XCURSOR_THEME", "bibata-modern-ice")
+hl.env("XCURSOR_SIZE", "24")
