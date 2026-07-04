b0743e1f464fe8a702699de2e702c5688b055d15
JoaoSant7 <pedro_romano7@proton.me>
2026-07-04 13:25:03 -0300

    hypr: add icc color profile to both builtin and external monitors

--- hyprland/.config/hypr/config/monitors.lua
+++ hyprland/.config/hypr/config/monitors.lua
@@ -5,14 +5,18 @@ hl.monitor({
 	mode = "preferred",
 	position = "0x0",
 	scale = "1",
+	icc = "/home/pedro/.local/share/icc/edid-263a4f42b4ec83145a521934d747f82b.icc",
+	bitdepth = 8,
 })
 
 hl.monitor({
 	output = "eDP-2",
 	disabled = true,
+	icc = "/home/pedro/.local/share/icc/edid-52036e7bf5f00ef980de0a9944c5c6f0.icc",
 })
 
 hl.monitor({
 	output = "eDP-1",
 	disabled = true,
+	icc = "/home/pedro/.local/share/icc/edid-52036e7bf5f00ef980de0a9944c5c6f0.icc",
 })
