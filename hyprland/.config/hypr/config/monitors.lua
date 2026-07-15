3f50189d8b13e31bf9aba48c8106fdce5dff5b71
JoaoSant7 <pedro_romano7@proton.me>
2026-07-15 14:44:22 -0300

    hyprland: fix the monitors configuration

--- hyprland/.config/hypr/config/monitors.lua
+++ hyprland/.config/hypr/config/monitors.lua
@@ -4,12 +4,25 @@ hl.monitor({
 	output = "HDMI-A-1",
 	mode = "preferred",
 	position = "0x0",
-	scale = "1",
+	scale = 1,
 	icc = "/home/pedro/.local/share/icc/edid-263a4f42b4ec83145a521934d747f82b.icc",
 	bitdepth = 8,
 })
 
 hl.monitor({
-	output = "Chimei Innolux Corporation 0x1522",
+	output = "eDP-1",
+	mode = "preferred",
+	position = "auto",
+	scale = 1.25,
 	icc = "/home/pedro/.local/share/icc/edid-52036e7bf5f00ef980de0a9944c5c6f0.icc",
+	bitdepth = 8,
+})
+
+hl.monitor({
+	output = "eDP-2",
+	mode = "preferred",
+	position = "auto",
+	scale = 1.25,
+	icc = "/home/pedro/.local/share/icc/edid-52036e7bf5f00ef980de0a9944c5c6f0.icc",
+	bitdepth = 8,
 })
