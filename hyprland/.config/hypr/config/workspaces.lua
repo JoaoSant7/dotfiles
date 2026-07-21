19f78c1fe96c5990116f0f8c527e403bf2e783c1
JoaoSant7 <pedro_romano7@proton.me>
2026-07-21 20:25:25 -0300

    add terminal scratchpad

--- hyprland/.config/hypr/config/workspaces.lua
+++ hyprland/.config/hypr/config/workspaces.lua
@@ -5,3 +5,12 @@ for i = 1, 4 do
 		default = (i == 1),
 	})
 end
+
+hl.workspace_rule({
+	workspace = "special:term",
+	gaps_out = 100,
+	persistent = false,
+	on_created_empty = "ghostty",
+})
+
+hl.bind("SUPER + apostrophe", hl.dsp.workspace.toggle_special("term"))
