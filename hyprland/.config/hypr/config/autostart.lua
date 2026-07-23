7c763819b70b57d140d66834dec872ea6dbf3336
JoaoSant7 <pedro_romano7@proton.me>
2026-07-22 21:15:39 -0300

    remove bibata

--- hyprland/.config/hypr/config/autostart.lua
+++ hyprland/.config/hypr/config/autostart.lua
@@ -11,4 +11,4 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("fcitx5 -d")
 end)
 
-hl.exec_cmd("hyprctl setcursor Bibita-Modern-Ice 24")
+--hl.exec_cmd("hyprctl setcursor Bibita-Modern-Ice 24")
