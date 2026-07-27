3c460a2e7a7157ca00145892eb33d98100f75824
JoaoSant7 <pedro_romano7@proton.me>
2026-07-26 21:41:38 -0300

    reduce column width

--- hyprland/.config/hypr/config/scrolling.lua
+++ hyprland/.config/hypr/config/scrolling.lua
@@ -1,6 +1,6 @@
 hl.config({
 	scrolling = {
-		column_width = 0.5,
+		column_width = 0.49,
 		fullscreen_on_one_column = true,
 		focus_fit_method = 1, -- 0 = center, 1 = fit
 		follow_focus = true,
