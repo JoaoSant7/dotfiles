26c08e89d7723700b9f3c2ed3137862c45c84d81
JoaoSant7 <pedro_romano7@proton.me>
2026-07-28 19:18:53 -0300

    fix column width

--- hyprland/.config/hypr/config/scrolling.lua
+++ hyprland/.config/hypr/config/scrolling.lua
@@ -5,7 +5,7 @@ hl.config({
 		focus_fit_method = 1, -- 0 = center, 1 = fit
 		follow_focus = true,
 		follow_min_visible = 1.0,
-		explicit_column_widths = "0.5, 1.0",
+		explicit_column_widths = "0.49, 1.0",
 		direction = "right",
 	},
 })
