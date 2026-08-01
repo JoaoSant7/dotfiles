b1cd281a1095ad99fc0aca01b1d213c6c2cafa95
JoaoSant7 <pedro_romano7@proton.me>
2026-08-01 14:06:33 -0300

    add env vars

--- hyprland/.config/hypr/config/environment.lua
+++ hyprland/.config/hypr/config/environment.lua
@@ -1,8 +1,29 @@
 -- Environmental variables
 -- Prefer UWSM
 
---hl.env("HYPRCURSOR_THEME", "bibata-modern-ice")
---hl.env("HYPRCURSOR_SIZE", "24")
+-- NVIDIA
+--hl.env("GBM_BACKEND", "nvidia_drm")
+--hl.env("LIBVA_DRIVER_NAME", "nvidia")
+--hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 
---hl.env("XCURSOR_THEME", "bibata-modern-ice")
---hl.env("XCURSOR_SIZE", "24")
+-- Electron
+--hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
+
+-- GDK
+--hl.env("GDK_BACKEND", "wayland")
+
+-- XDG
+--hl.env("XDG_SESSION_TYPE", "wayland")
+
+-- Qt
+--hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+--hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+--hl.env("QT_QPA_PLATFORM", "wayland")
+
+-- Cursor
+--hl.env("XCURSOR_THEME", "Posy_Cursor")
+--hl.env("XCURSOR_SIZE", "32")
+
+-- Hyprcursor
+--hl.env("HYPRCURSOR_THEME", "Posy_Cursor")
+--hl.env("HYPRCURSOR_SIZE", "32")
