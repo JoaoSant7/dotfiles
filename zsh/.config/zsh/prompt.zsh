a45d6f3016ae7b79e3ed1e810df82d151a4e1375
JoaoSant7 <pedro_romano7@proton.me>
2026-08-02 13:47:41 -0300

    use starship

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -3,7 +3,7 @@
 # Prevent Python virtualenv from polluting the prompt
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
-#eval "$(starship init zsh)"
+eval "$(starship init zsh)"
 
 #source /home/linuxbrew/.linuxbrew/share/powerlevel10k/powerlevel10k.zsh-theme
 
