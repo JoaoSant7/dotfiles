f153ef2e67d0eae5a17406d71ead3ec68165d554
JoaoSant7 <pedro_romano7@proton.me>
2026-08-31 20:19:00 -0300

    update variables for hyprland

--- hyprland/.config/hypr/config/environment.lua
+++ hyprland/.config/hypr/config/environment.lua
@@ -2,28 +2,28 @@
 -- Prefer UWSM
 
 -- NVIDIA
---hl.env("GBM_BACKEND", "nvidia_drm")
---hl.env("LIBVA_DRIVER_NAME", "nvidia")
---hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("GBM_BACKEND", "nvidia_drm")
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 
 -- Electron
---hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
 -- GDK
---hl.env("GDK_BACKEND", "wayland")
+hl.env("GDK_BACKEND", "wayland")
 
 -- XDG
---hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_TYPE", "wayland")
 
 -- Qt
---hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
---hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
---hl.env("QT_QPA_PLATFORM", "wayland")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_QPA_PLATFORM", "wayland")
 
 -- Cursor
---hl.env("XCURSOR_THEME", "Posy_Cursor")
---hl.env("XCURSOR_SIZE", "32")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
+hl.env("XCURSOR_SIZE", "24")
 
 -- Hyprcursor
---hl.env("HYPRCURSOR_THEME", "Posy_Cursor")
---hl.env("HYPRCURSOR_SIZE", "32")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
+hl.env("HYPRCURSOR_SIZE", "24")
