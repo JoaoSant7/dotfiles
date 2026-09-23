72872eea2da127ca4e3d7588cf81983ed3cab527
JoaoSant7 <pedro_romano7@proton.me>
2026-09-22 21:19:14 -0300

    comment monitors config

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -1,6 +1,6 @@
 -- Hyprland Configuration
 
-require("config.auto-monitors")
+--require("config.auto-monitors")
 require("config.animations")
 require("config.autostart")
 require("config.colors")
@@ -11,7 +11,7 @@ require("config.input")
 require("config.binds")
 require("config.misc")
 require("config.scrolling")
-require("config.monitors")
+--require("config.monitors")
 require("config.windowrules")
 require("config.workspaces")
 
