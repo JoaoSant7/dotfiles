a797faad82f32098ad2c1869089653da2ae7d988
JoaoSant7 <pedro_romano7@proton.me>
2026-09-23 18:57:02 -0300

    remove flag for kanshi

--- hyprland/.config/hypr/config/autostart.lua
+++ hyprland/.config/hypr/config/autostart.lua
@@ -14,6 +14,6 @@
 
 hl.on("hyprland.start", function()
     hl.exec_cmd("uwsm app -- fcitx5 -d")
-    hl.exec_cmd("uwsm app -- kanshi")
+--    hl.exec_cmd("uwsm app -- kanshi")
     hl.exec_cmd("uwsm app -- noctalia")
 end)
