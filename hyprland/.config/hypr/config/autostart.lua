2cef6a09b3da0b985d0b2b127a36077a691c7290
JoaoSant7 <pedro_romano7@proton.me>
2026-09-23 14:53:15 -0300

    fix the autostart string

--- hyprland/.config/hypr/config/autostart.lua
+++ hyprland/.config/hypr/config/autostart.lua
@@ -12,10 +12,8 @@
 --end)
 --
 
-hl.exec_cmd("uwsm app -- fcitx5 -d")
-
-hl.exec_cmd("kanshi")
-
 hl.on("hyprland.start", function()
+    hl.exec_cmd("uwsm app -- fcitx5 -d")
+    hl.exec_cmd("uwsm app -- kanshi")
     hl.exec_cmd("uwsm app -- noctalia")
 end)
