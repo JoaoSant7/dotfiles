2d6a8e31c74752f007009c6fb230b225b39f9ccd
JoaoSant7 <pedro_romano7@proton.me>
2026-09-23 22:40:02 -0300

    remove uwsm flags

--- hypr/.config/hypr/config/environment.lua
+++ hypr/.config/hypr/config/environment.lua
@@ -7,22 +7,3 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'")
     hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-size 24")
 end)
-
--- NVIDIA
---hl.env("GBM_BACKEND", "nvidia_drm")
---hl.env("LIBVA_DRIVER_NAME", "nvidia")
---hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-
--- Electron
---hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
-
--- GDK
---hl.env("GDK_BACKEND", "wayland")
-
--- XDG
---hl.env("XDG_SESSION_TYPE", "wayland")
-
--- Qt
---hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
---hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
---hl.env("QT_QPA_PLATFORM", "wayland")
