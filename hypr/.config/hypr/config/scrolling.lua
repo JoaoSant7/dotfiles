b8748c6d25d0c2197a1613ea13e74a8bcbfe95e6
JoaoSant7 <pedro_romano7@proton.me>
2026-09-24 08:27:19 -0300

    reduce follow min visible

--- hypr/.config/hypr/config/scrolling.lua
+++ hypr/.config/hypr/config/scrolling.lua
@@ -4,7 +4,7 @@ hl.config({
 		fullscreen_on_one_column = true,
 		focus_fit_method = 1, -- 0 = center, 1 = fit
 		follow_focus = true,
-		follow_min_visible = 1.0,
+		follow_min_visible = 0.4,
 		explicit_column_widths = "0.49, 1.0",
 		direction = "right",
 	},
