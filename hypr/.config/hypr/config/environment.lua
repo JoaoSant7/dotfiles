73b80eeb84e3d94ab5239a531b3d38ea22a5ce35
JoaoSant7 <pedro_romano7@proton.me>
2026-09-23 21:55:54 -0300

    add potential fix to cursor inconsistency

--- hypr/.config/hypr/config/environment.lua
+++ hypr/.config/hypr/config/environment.lua
@@ -1,6 +1,13 @@
 -- Environmental variables
 -- Prefer UWSM
 
+-- Startup (runs once per session)
+hl.on("hyprland.start", function()
+    hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 24")
+    hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'")
+    hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-size 24")
+end)
+
 -- NVIDIA
 --hl.env("GBM_BACKEND", "nvidia_drm")
 --hl.env("LIBVA_DRIVER_NAME", "nvidia")
@@ -19,11 +26,3 @@
 --hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 --hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 --hl.env("QT_QPA_PLATFORM", "wayland")
-
--- Cursor
---hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
---hl.env("XCURSOR_SIZE", "24")
-
--- Hyprcursor
---hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
---hl.env("HYPRCURSOR_SIZE", "24")
