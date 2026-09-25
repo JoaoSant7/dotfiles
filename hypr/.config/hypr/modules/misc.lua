2f96611208e0758e8e52a256ab8159f5dbc28ad9
JoaoSant7 <pedro_romano7@proton.me>
2026-09-25 19:56:20 -0300

    add more flags to misc

--- hypr/.config/hypr/modules/misc.lua
+++ hypr/.config/hypr/modules/misc.lua
@@ -1,10 +1,8 @@
 hl.config({
 	misc = {
-		col = {
-			splash = "0xff95C561",
-		},
 		disable_hyprland_logo = true,
 		disable_splash_rendering = true,
+		disable_scale_notification = true,
 		middle_click_paste = false,
 		enable_swallow = true,
 		swallow_regex = "(kitty|ghostty|[Kk]onsole|Alacritty|gnome-terminal|xfce[0-9]?-terminal)",
@@ -12,12 +10,13 @@ hl.config({
 		-- Startup wallpaper
 		force_default_wallpaper = 0,
 	},
+
 	xwayland = {
 		force_zero_scaling = true,
 	},
+
 	ecosystem = {
 		no_update_news = true,
 		no_donation_nag = true,
 	},
 })
-
